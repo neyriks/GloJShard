ecfcdd62d77a542a8373ca1f9e09cdd62993feca
neyriks <mesuppgg@gmail.com>
2020-03-30 20:33:21 +0300

    Решения задачи
    Добавил решения для задачи #1 - третьего урока. (Усложненный)

--- main.js
+++ main.js
@@ -10,20 +10,39 @@ numRoot = result ** 3;
 console.log(String(numRoot).slice(0,2));
 
 
-// Задача календарь (ЗАДАЧА 3 УСЛОЖНЕННАЯ)
+// Задача календарь (ЗАДАЧА 3 УСЛОЖНЕННАЯ) switch
 
+// let lang = prompt('Введите язык: ru или en');
+// switch (lang) {
+// 	case 'ru':
+// 		arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+// 	break;
+// 	case 'en':
+// 		arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+//   break;
+//   default:
+//     console.log('something wrong');
+// }
+// console.log(arr);
+
+// Через if
+// let lang = prompt('Введите язык: ru или en');
+// if (lang === 'ru') {
+//   arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+// } else if (lang === 'en') {
+//   arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+// } else {
+//   console.log('wrong language');
+// } 
+// console.log(arr);
+
+// Многомерный массив.
 let lang = prompt('Введите язык: ru или en');
-switch (lang) {
-	case 'ru':
-		arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-	break;
-	case 'en':
-		arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
-  break;
-  default:
-    console.log('something wrong');
-}
-console.log(arr);
+let array = {
+	'ru':['пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс'],
+	'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
+};
+console.log(array[lang]);
 
 // Задача с namePerson (ЗАДАЧА 3 УСЛОЖНЕННАЯ)
 
