2261dccaa898e1a2e126aaad4976bc9622d90e8d
neyriks <mesuppgg@gmail.com>
2020-05-07 01:06:19 +0300

    Урок 5
    1) Создать массив arr = []
    
    — Записать в него 7 любых многозначных чисел в виде строк
    
    — Вывести в консоль только те, что начинаются с цифры 2 или 4 (Должны присутствовать в массиве)
    
    2) Вывести в столбик все простые числа от 1 до 100
    
    — Статья про простые числа - КЛИК
    
    — Рядом с каждым числом написать оба делителя данного числа
    
        Например: “Делители этого числа: 1 и n”

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 'use strict';
-
-let array = ['227', '258', '411', '443', '559', '661', '260'];
-array.forEach((number) => {                                   // Использовал стрелочную функцию.
-    if (number.startsWith('2') || number.startsWith('4')) {   // Указываем с какой цифры стартуем. (отвечаем startsWith)
-      console.log(number);                                    // Выводим цифры в консоль.
+let arr = [
+    '233','241','395','446','922','513','421'
+];
+arr.forEach(e => {
+    if(e[0] === '2' || e[0] === '4') {   // Можно было черех startWith
+        console.log(e);
     }
-  });
+});
+
+// Простые числа.
+foo: // Метка
+for (let i = 2; i < 100; i++) {
+    for(let j = 2; j < i; j++) {
+        if(i % j === 0){
+            continue foo; 
+        }
+    }
+    console.log(i + ' - Делители этого числа: 1 и n = ' + i);
+}
