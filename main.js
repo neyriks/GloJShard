e674225bea387bfc32b766aba6d2ec5184fa0823
neyriks <mesuppgg@gmail.com>
2020-05-07 01:52:04 +0300

    Урок 18
    Поправил анимацию квадрата с кнопки.

--- main.js
+++ main.js
@@ -1,34 +1,40 @@
 'use strict';
-
-const button = document.getElementById('button'),
-     reset = document.getElementById('reset'),
-     block = document.querySelector('.block');
-let intervalBlock,
-     count = 0,
-     animate = false;
-let move = function() {
-    intervalBlock = requestAnimationFrame(move);
-    count ++;
-    if (count < 350) {
-        block.style.left = count + "px";
-    } else {
-        cancelAnimationFrame(intervalBlock);
-    }
-};
-
-button.addEventListener('click', () => {
-    if(animate) {
-        intervalBlock = requestAnimationFrame(move);
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('button'),
+        reset = document.getElementById('reset'),
+        block = document.querySelector('.block');
+    let intervalBlock,
+        count = 0,
         animate = false;
-    } else {
-        animate = true;
+    button.addEventListener('click', () => {
+        if (animate) {
+            intervalBlock = requestAnimationFrame(move);
+            animate = false;
+        } else if (!animate) {
+            animate = true;
+            intervalBlock = requestAnimationFrame(move);
+        }
+        else {
+            animate = true;
+            cancelAnimationFrame(intervalBlock);
+        }
+
+    });
+    let move = function () {
+        intervalBlock = requestAnimationFrame(move);
+        count++;
+        if (count < 350) {
+            block.style.left = count + "px";
+        } else {
+            cancelAnimationFrame(intervalBlock);
+        }
+    };
+
+
+    reset.addEventListener('click', () => {
+        count = 0;
+        animate = false;
+        block.style.left = 0;
         cancelAnimationFrame(intervalBlock);
-    }
-    
-});
-reset.addEventListener('click', () => {
-    count = 0;
-    animate = false;
-    block.style.left = 0;
-    cancelAnimationFrame(intervalBlock);
-});
+    });
+})
